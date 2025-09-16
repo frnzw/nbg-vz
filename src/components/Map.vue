41c476a72bece35344be1f4f24dc70da513bbcfb
fw <frnzwendt@gmail.com>
2025-09-16 16:50:49 +0200

    Initial commit: leaflet map as view component

---
+++ src/components/Map.vue
@@ -0,0 +1,39 @@
+<script setup>
+  import { onMounted, ref } from 'vue'
+  import "leaflet/dist/leaflet.css";
+  import L from "leaflet";
+
+  const center = [37.7749, -122.4194];
+  const initMap = function() {
+    const map = L.map("mapContainer").setView(center, 13);
+    const tileLayer = L.tileLayer(
+        'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}' + (L.Browser.retina ? '@2x.png' : '.png'), 
+        {
+          attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+          subdomains: 'abcd',
+          maxZoom: 20,
+          minZoom: 0
+    });
+
+    tileLayer.addTo(map)
+  }
+
+  onMounted(initMap);
+</script>
+
+<template>
+  <div id="container">
+    <h1>Vue + leaflet starter</h1>
+    <div id="mapContainer">
+
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+#mapContainer {
+ width: 90vw;
+ height: 80vh;
+}
+</style>
