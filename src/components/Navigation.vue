79c83425674f84421685ad4cac0782d4c68bf835
fw <frnzwendt@gmail.com>
2025-11-03 09:19:02 +0100

    cleanup

--- src/components/Navigation.vue
+++
@@ -1,8 +0,0 @@
-<script setup></script>
-<template>
-  <v-tabs :model-value="route.path">
-    <v-tab to="/map/places" value="/map/places"> Places View </v-tab>
-    <v-tab to="/map/persons" value="/map/persons"> Persons View </v-tab>
-    <v-tab to="/map/distant" value="/map/persons"> Distant View </v-tab>
-  </v-tabs>
-</template>
