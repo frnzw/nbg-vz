89d11d82a98736f1786b8d734cedbd2ff935b2fb
fw <frnzwendt@gmail.com>
2025-11-08 14:07:57 +0100

    docs for aborted map refactoring

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,5 +1,17 @@
 <script setup>
-import Map from "../components/Map.vue";
+  import Map from '../components/Map.vue';
+
+  /**
+   * @component MapView
+   * A "View" component that serves as the route-level wrapper for all `/map` pages.
+   *
+   * Its only job at the moment is to render the <Map> component, which contains the actual content.
+   * This follows the "View/Component" pattern. In the future, the Map Component should be stripped of
+   * all logic combining other componentes and rendering them according to route. Instead, this logic should be
+   * moved into this View component, as that is the better place for combining components in the View/Component pattern.
+   * First attempts to achieve this were aborted after experiencing runtime issues with the Leaflet map
+   * not being properly accessible on Zoom events (see reverted commit e30d31d24a74bc422f9ddf352a2c379e8f56695e).
+   */
 </script>
 
 <template>
